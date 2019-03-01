243eb47e1661f00ada3a7f01f8a8cad06a61fdd2
shivammutreja <shivammutreja25@gmail.com>
2019-03-01 17:37:49 +0530

    changed hostname

--- src/main.js
+++ src/main.js
@@ -14,7 +14,9 @@ Vue.http.options.emulateJSON = true
 
 // Vue.prototype.$hostname = "https://qcitech.org:8082/inventory/"
 
-Vue.prototype.$hostname = 'https://api-inventory.qcitech.org/';
+// Vue.prototype.$hostname = 'https://api-inventory.qcitech.org/';
+
+Vue.prototype.$hostname = 'https://api-inventory.qcin.org/';
 
 // Vue.prototype.$hostname = "http://192.168.15.153:5000/inventory/";
 // var token = localStorage.getItem('token')
